04477096f41adc7e0306625c7c1fd3820b3ff0e0
unknown <klatmddud@gmail.com>
2026-04-23 15:32:18 +0900

    dd

--- scripts/bash/hard_replay_only/hard_replay_only_seed_mean.bash
+++ scripts/bash/hard_replay_only/hard_replay_only_seed_mean.bash
@@ -14,7 +14,7 @@ MODULE_BASE_PATH="scripts/bash/$MOD/cfg"
 MDMB_CFG="$MODULE_BASE_PATH/mdmb_off.yaml"
 MDMBPP_CFG="$MODULE_BASE_PATH/mdmbpp_on.yaml"
 HARD_REPLAY_CFG="$MODULE_BASE_PATH/hard_replay_on.yaml"
-RASD_CFG="$MODULE_BASE_PATH/rasd_on.yaml"
+RASD_CFG="$MODULE_BASE_PATH/rasd_off.yaml"
 
 DEVICE="${DEVICE:-cuda:0}"
 LOG_DIR="scripts/bash/$MOD/logs"
